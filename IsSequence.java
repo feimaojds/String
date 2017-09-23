d31135dcfb12443ba25947994b30b08a1e956f4a
feimaojds <313061793@qq.com>
2017-09-23 17:16:07 +0800

    Given a string s and a string t, check if s is subsequence of t.
    You may assume that there is only lower case English letters in both s and t. t is potentially a very long (length ~= 500,000) string, and s is a short string (<=100).
    
    A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
    
    Example 1:
    s = "abc", t = "ahbgdc"
    
    Return true.
    
    Example 2:
    s = "axc", t = "ahbgdc"
    
    Return false.
    
    Follow up:
    If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
    
    思路：设置s的索引indexS，遍历t字符串。
               先在t中找s的第一个字符，找到后找第二个字符……

---
+++ IsSequence.java
@@ -0,0 +1,18 @@
+package leecode;
+
+public class IsSequence {
+
+	
+public boolean isSubsequence(String s, String t) {
+	    if(s.length() == 0) return true;
+	    if(t.length() == 0) return false;
+        int indexS = 0;
+        for(int i=0;i<t.length();i++){
+        	if(t.charAt(i) == s.charAt(indexS)){
+        		indexS++;
+        	}
+        	if(indexS==s.length()) return true;
+        }
+        return false;
+    }
+}
